b0669a4f6d2958c1d26142f44e160adb4c95ce64
Abdulmalik Muhammad <abdulmalik.m.m@nuprc.gov.ng>
2025-07-22 17:28:35 +0100

    Filling the collection of measurement units

--- MauiVerter/ViewModels/ConverterPageViewModel.cs
+++ MauiVerter/ViewModels/ConverterPageViewModel.cs
@@ -1,5 +1,27 @@
-﻿namespace MauiVerter.ViewModels;
+﻿using System.Collections.ObjectModel;
+using UnitsNet;
+
+namespace MauiVerter.ViewModels;
 
 public class ConverterPageViewModel
 {
+    public string QuantityName { get; set; } = "Length";
+    public ObservableCollection<string> FromMeasures { get; set; } = [];
+    public ObservableCollection<string> ToMeasures { get; set; } = [];
+    public string CurrentFromMeasure { get; set; } = "Meter";
+    public string CurrentToMeasure { get; set; } = "Centimeter";
+
+    public ConverterPageViewModel()
+    {
+        ObservableCollection<string> measures = LoadMeasures();
+        FromMeasures = measures;
+        ToMeasures = measures;
+    }
+
+    private ObservableCollection<string> LoadMeasures() =>
+        new(
+            Quantity.Infos
+            .FirstOrDefault(x => x.Name == QuantityName)!
+            .UnitInfos
+            .Select(u => u.Name));
 }
